940ebc3de22e78e05ce0eaaf6954f9e834dea758
pierre golfier <pgolfier.pro@gmail.com>
2024-01-21 11:31:07 +0100

    refacto(game): dev config handling

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@
 import { PhysicDebuggerModes } from './app/engine'
 import { Game } from './app/game'
 
+const devConfig: {
+  physicsDebugger: PhysicDebuggerModes
+  controls: 'map' | 'tps'
+} = {
+  physicsDebugger: PhysicDebuggerModes.On,
+  controls: 'map',
+}
+
 new Game({
   engine: {
     debugUi: import.meta.env.DEV,
-    physicsDebugger: import.meta.env.DEV ? PhysicDebuggerModes.On : PhysicDebuggerModes.Off,
+    physicsDebugger: import.meta.env.DEV ? devConfig.physicsDebugger : PhysicDebuggerModes.Off,
   },
-  controls: 'map',
+  controls: import.meta.env.DEV ? devConfig.controls : 'tps',
 })
